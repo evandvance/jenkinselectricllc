f6597927519247901a4c173a6614344eead67983
Evan <evandvance@gmail.com>
2024-05-20 13:44:01 -0500

    Begining of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Navbar = () => {
-  return <nav data-testid="nav-1">Navbar</nav>;
+  return (
+    <nav
+      data-testid="nav-1"
+      className="bg-slate-950 min-h-[10vh] w-screen flex flex-col items-center justify-around"
+    >
+      <Link href="/" className="h-[10vh] w-28 fixed">
+        <Image
+          className="object-contain"
+          src="assets/logo.svg"
+          alt="Jenkins Electric LLC logo"
+          fill
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
