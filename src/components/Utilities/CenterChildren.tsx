eb2c80d7c0ef7d1ceff3187119001d3601646ca2
Evan <evandvance@gmail.com>
2024-05-20 10:08:35 -0500

    Added Center Children Utility Component

---
+++ src/components/Utilities/CenterChildren.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+interface CenterChildrenProps {
+  children: ReactNode;
+  className?: string;
+  key?: string;
+}
+
+const CenterChildren = ({ children, className, key }: CenterChildrenProps) => {
+  return (
+    <div key={key} className={`flex justify-center items-center ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default CenterChildren;
