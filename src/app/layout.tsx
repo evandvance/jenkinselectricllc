5140cfcaa355be586889438d3d867fe7566dc0df
Evan <evandvance@gmail.com>
2024-05-22 09:54:15 -0500

    Added roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({ weight: '500', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Jenkins Electric LLC',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
