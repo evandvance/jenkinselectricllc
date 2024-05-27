b89ee555e99f28541e0db8632e52bdd49c8555a7
Evan <evandvance@gmail.com>
2024-05-27 10:32:31 -0500

    Fixed spacing and missing /

--- src/components/Cards/ComingSoonCard.tsx
+++ src/components/Cards/ComingSoonCard.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image';
 const ComingSoonCard = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center mt-10">
       <Image
-        src={'assets/comingsoonlogo.jpg'}
+        src={'/assets/comingsoonlogo.jpg'}
         alt="An Image of the JELLC Logo"
         height={500}
         width={500}
       />
 
-      <h1 className="text-8xl">Page Coming Soon!</h1>
+      <h1 className="text-8xl mt-24">Page Coming Soon!</h1>
     </div>
   );
 };
