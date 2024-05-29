d80f123b02758a14da55cc3cb5ce2d1c161f5503
Evan <evandvance@gmail.com>
2024-05-29 11:10:50 -0500

    Added route for getting all technicians

---
+++ src/app/api/technicians/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const technicians = await prisma.technician.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        phoneNumber: true,
+        bio: true,
+        isCertified: true,
+        image: true,
+      },
+    });
+
+    return NextResponse.json(technicians);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: 'An error has occured' },
+      { status: 500 }
+    );
+  }
+}
