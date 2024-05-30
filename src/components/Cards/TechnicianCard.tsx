7d2d63094762d9b8cd29532d178be808bfe53c4a
Evan <evandvance@gmail.com>
2024-05-30 11:02:59 -0500

    Made Technician Card

---
+++ src/components/Cards/TechnicianCard.tsx
@@ -0,0 +1,50 @@
+import { FaAward } from 'react-icons/fa6';
+
+interface TechnicianCardProps {
+  firstName: string;
+  lastname: string;
+  phoneNumber?: string;
+  bio: string;
+  isCertified: boolean;
+  imageUrl: string;
+  index: number;
+}
+
+const isEven = (index: number): boolean => {
+  return index % 2 === 0;
+};
+
+const TechnicianCard = ({
+  firstName,
+  lastname,
+  phoneNumber,
+  bio,
+  isCertified,
+  imageUrl,
+  index,
+}: TechnicianCardProps) => {
+  const gradient = isEven(index)
+    ? 'bg-gradient-to-r from-jellcdarkblue to-jellcblue'
+    : 'bg-gradient-to-l from-jellcyellow to-jellcorange';
+
+  return (
+    <div
+      className={`w-[75vw] m-5 p-5 border rounded-xl text-white flex flex-col lg:flex-row ${gradient}`}
+    >
+      <div className="w-1/2 flex justify-center items-center">
+        <img
+          className="h-[500px]"
+          src={imageUrl}
+          alt={`Image of ${firstName} ${lastname}`}
+        />
+        {isCertified && <FaAward color="white" height={50} />}
+      </div>
+      <div className="w-1/2 flex flex-col justify-center items-start">
+        <h2 className="text-5xl m-5">{`${firstName} ${lastname}`}</h2>
+        <p className="text-2xl m-5">{bio}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TechnicianCard;
