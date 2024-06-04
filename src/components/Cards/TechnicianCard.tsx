db58c6aa7623781279ec0985df8d31188f95eeb3
Evan <evandvance@gmail.com>
2024-06-04 12:44:10 -0500

    Fixed technician card images

--- src/components/Cards/TechnicianCard.tsx
+++ src/components/Cards/TechnicianCard.tsx
@@ -27,24 +27,25 @@ const TechnicianCard = ({
 }: TechnicianCardProps) => {
   const gradient = isEven(index)
     ? 'bg-gradient-to-r from-jellcdarkblue to-jellcblue'
-    : 'bg-gradient-to-l from-jellcorange to-jellcyellow flex-row-reverse';
+    : 'bg-gradient-to-r from-jellcorange to-jellcyellow lg:flex-row-reverse';
 
   return (
     <div
-      className={`w-5/6 lg:w-3/4 m-5 p-5 border rounded-xl text-white flex flex-col lg:flex-row ${gradient}`}
+      className={`w-7/8 lg:w-3/4 m-5 p-5 border rounded-xl text-white flex flex-col lg:flex-row ${gradient}`}
       key={key}
     >
-      <div className="w-full lg:w-1/2 flex justify-center items-center">
+      <div className="w-full lg:min-w-1/2 flex justify-center items-center relative">
         <Image
-          width={400}
           height={500}
+          width={600}
+          className="object-cotain w-full h-auto p-5"
           src={imageUrl}
           alt={`Image of ${firstName} ${lastName}`}
         />
       </div>
-      <div className="min-w-1/2 flex flex-col justify-center items-start">
+      <div className="flex flex-col justify-center items-start">
         <div className="text-5xl my-5 w-content flex lg:items-center">
-          <h2 className="lg:mx-4">{`${firstName} ${lastName}`}</h2>
+          <h2 className="lg:mx-5">{`${firstName} ${lastName}`}</h2>
           {isCertified && (
             <div className="flex flex-col items-center">
               <FaAward className="text-7xl lg:mx-4" color="white" />
@@ -52,7 +53,7 @@ const TechnicianCard = ({
             </div>
           )}
         </div>
-        <p className="text-xl lg:text-2xl my-5">{bio}</p>
+        <p className="text-xl lg:text-2xl m-5">{bio}</p>
       </div>
     </div>
   );
