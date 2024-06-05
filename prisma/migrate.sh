2026553ec683efce1cfe35e99094dc52aa4c5354
Evan <evandvance@gmail.com>
2024-06-05 12:29:33 -0500

    Add Script to automate increasing migration count

--- prisma/migrate.sh
+++ prisma/migrate.sh
@@ -5,7 +5,9 @@ DATABASE_PORT=5432
 
 DATABASE_URL=postgresql://postgres:${POSTGRES_PASSWORD}@localhost:${DATABASE_PORT}/postgres
 
-migration_number=2
+migration_number= find ./prisma/migrations -mindepth 1 -maxdepth 1 -type d | wc -l
+
+migration_number = $migration_number + 1
 
 docker run -dp 5432:5432 -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_USER=${POSTGRES_USER} --name temp_db postgres:16-alpine 
 
