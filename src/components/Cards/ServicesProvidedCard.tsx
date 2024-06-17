e8bf941a3a56dca76f34f11437b645231d4ad655
Evan <evandvance@gmail.com>
2024-06-17 08:56:06 -0500

    Fix services proved card display issues

--- src/components/Cards/ServicesProvidedCard.tsx
+++ src/components/Cards/ServicesProvidedCard.tsx
@@ -17,24 +17,26 @@ const ServicesProvidedCard = ({ className }: ServicesProvidedCardProps) => {
   ];
 
   return (
-    <div
-      data-testid="servicesProvidedCard-1"
-      id="services"
-      className={`w-[80vw] h-content m-5 ${className ? className : ''}`}
-    >
-      <h2 className="text-5xl m-5">Services Provided</h2>
-      <div className="flex flex-col justify-around items-center lg:flex-row">
-        <ul className="text-2xl lg:text-4xl my-5 list-disc space-y-1 lg:space-y-2">
-          {servicesArray.map((service) => (
-            <li key={service}>{service}</li>
-          ))}
-        </ul>
-        <Image
-          src="/images/vehicles.jpg"
-          alt="Image of vehicles parked in front of a garage"
-          height={500}
-          width={500}
-        />
+    <div className="w-screen bg-white flex justify-center">
+      <div
+        data-testid="servicesProvidedCard-1"
+        id="services"
+        className={`w-[80vw] h-content m-5 ${className ? className : ''}`}
+      >
+        <h2 className="text-5xl m-5">Services Provided</h2>
+        <div className="flex flex-col justify-around items-center lg:flex-row">
+          <ul className="text-2xl lg:text-4xl my-5 list-disc space-y-1 lg:space-y-2">
+            {servicesArray.map((service) => (
+              <li key={service}>{service}</li>
+            ))}
+          </ul>
+          <Image
+            src="/images/vehicles.jpg"
+            alt="Image of vehicles parked in front of a garage"
+            height={500}
+            width={500}
+          />
+        </div>
       </div>
     </div>
   );
