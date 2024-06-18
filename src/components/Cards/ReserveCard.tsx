c98ab27ab97a5e87c72419e7001391d57cf60d9c
Evan <evandvance@gmail.com>
2024-06-18 10:21:23 -0500

    Add error handling for reserving nonexistent appliances

--- src/components/Cards/ReserveCard.tsx
+++ src/components/Cards/ReserveCard.tsx
@@ -54,49 +54,56 @@ const ReserveCard = ({ id }: ReserveCardProps) => {
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-5 flex flex-col justify-center items-center w-[90%] p-5 border rounded-xl bg-gradient-to-r from-jellcdarkblue to-jellcblue text-white lg:w-3/4 space-y-5"
-      >
-        <h2 className="text-3xl">Reserve {appliance?.applianceName}</h2>
-        <div className="flex flex-col w-3/4 space-y-2">
-          <label className="text-xl" htmlFor="email">
-            Email
-          </label>
-          <input
-            {...register('email')}
-            className="rounded p-2 text-black"
-            type="text-area"
-            name="email"
-            id="email"
-          />
-          {errors.email && (
-            <p className="text-red-500">{errors.email.message}</p>
-          )}
-        </div>
-        <div className="flex flex-col w-3/4 space-y-2">
-          <label className="text-xl" htmlFor="comments">
-            Comments (optional)
-          </label>
-          <input
-            {...register('comments')}
-            className="rounded p-2 text-black"
-            type="text-area"
-            name="comments"
-            id="comments"
-          />
-          {errors.comments && (
-            <p className="text-red-500">{errors.comments.message}</p>
-          )}
-        </div>
-
-        <button
-          className="flex justify-center text-2xl items-center m-5 h-16 w-56 bg-white text-black rounded-xl "
-          type="submit"
+      {appliance ? (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-5 flex flex-col justify-center items-center w-[90%] p-5 border rounded-xl bg-gradient-to-r from-jellcdarkblue to-jellcblue text-white lg:w-3/4 space-y-5"
         >
-          Submit
-        </button>
-      </form>
+          <h2 className="text-3xl">Reserve {appliance?.applianceName}</h2>
+          <div className="flex flex-col w-3/4 space-y-2">
+            <label className="text-xl" htmlFor="email">
+              Email
+            </label>
+            <input
+              {...register('email')}
+              className="rounded p-2 text-black"
+              type="text-area"
+              name="email"
+              id="email"
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col w-3/4 space-y-2">
+            <label className="text-xl" htmlFor="comments">
+              Comments (optional)
+            </label>
+            <input
+              {...register('comments')}
+              className="rounded p-2 text-black"
+              type="text-area"
+              name="comments"
+              id="comments"
+            />
+            {errors.comments && (
+              <p className="text-red-500">{errors.comments.message}</p>
+            )}
+          </div>
+
+          <button
+            className="flex justify-center text-2xl items-center m-5 h-16 w-56 bg-white text-black rounded-xl "
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      ) : (
+        <div className="text-5xl mt-2 text-red-500">
+          {' '}
+          Requested appliance not found
+        </div>
+      )}
 
       {uploadError && (
         <div className="text-2xl lg:text-4xl text-red-500 text-center w-[90%] lg:w-3/4">
