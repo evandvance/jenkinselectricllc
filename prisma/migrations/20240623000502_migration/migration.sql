ec0a0990e2c95470cc77305a74f15826216b7607
evandvance <evandvance@gmail.com>
2024-06-22 19:05:26 -0500

    Change to schema for new reservation format

---
+++ prisma/migrations/20240623000502_migration/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `number` on the `reservations` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "reservations" DROP COLUMN "number";
--- src/interfaces/ReserveFormSchema.ts
+++ src/interfaces/ReserveFormSchema.ts
@@ -1,8 +1,19 @@
 import { z } from 'zod';
 
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
+
 export const reserveFormSchema = z.object({
   email: z.string().email(),
+  firstName: z.string(),
+  lastName: z.string(),
+  phoneNumber: z.string().regex(phoneRegex, 'Invalid Phone Number.'),
   comments: z.string(),
+  street: z.string().min(3).max(50),
+  city: z.string().min(3).max(50),
+  state: z.string().min(2).max(2),
+  zip: z.string().regex(new RegExp(/^\d{5}$/), 'Invalid Zip Code'),
 });
 
 export type ReserveFormData = z.infer<typeof reserveFormSchema>;
