38b9a2c446f95f1d6ca08556c15820fb73d286fd
evandvance <evandvance@gmail.com>
2024-06-24 14:46:33 -0500

    remove unused code

--- database/upsert_technicians.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Technician 
-('first_name', 'last_name', 'bio', 'isCertified', 'image_url') 
-VALUES
-('Dylan', 'Pride', '', 0, '' ),
-('Dylan', 'Pride', '', 0, '' ),
-('Dylan', 'Pride', '', 0, '' ),
-('Dylan', 'Pride', '', 0, '' ),
-('Dylan', 'Pride', '', 0, '' )
-ON CONLFICT
