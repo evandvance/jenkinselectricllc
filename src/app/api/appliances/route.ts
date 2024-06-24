504fb4d53279c91be942012d35d2bb27b1592d0f
evandvance <evandvance@gmail.com>
2024-06-24 16:13:02 -0500

    Add begining of pagination in api

--- src/app/api/appliances/route.ts
+++ src/app/api/appliances/route.ts
@@ -5,23 +5,36 @@ import { isApplianceType } from '@/interfaces/ApplianceInterface';
 import { ApplianceSelectConfig } from '@/configs/SelectConfigs';
 
 export async function GET(req: NextRequest) {
-  const applianceType = new URL(req.url).searchParams.get(
-    'type'
-  ) as ApplianceTypes;
+  const url = new URL(req.url);
+
+  const page = url.searchParams.get('page')
+    ? parseInt(url.searchParams.get('page')!)
+    : 0;
+
+  const pageSize = url.searchParams.get('size')
+    ? parseInt(url.searchParams.get('size')!)
+    : 25;
+
+  const applianceType = url.searchParams.get('type') as ApplianceTypes;
+
+  const skip = page > 1 ? page * pageSize - pageSize : 0;
+  const take = pageSize;
 
   if (applianceType) {
     return NextResponse.json(
       {
         message: 'Success',
         status: 200,
-        data: await getByType(applianceType),
+        data: await getByType(applianceType, take, skip),
       },
-      { status: 400 }
+      { status: 200 }
     );
   }
 
   const appliances = await prisma.appliances.findMany({
     select: ApplianceSelectConfig,
+    take,
+    skip,
   });
 
   return NextResponse.json(
@@ -34,7 +47,11 @@ export async function GET(req: NextRequest) {
   );
 }
 
-async function getByType(applianceType: ApplianceTypes) {
+async function getByType(
+  applianceType: ApplianceTypes,
+  take: number,
+  skip: number
+) {
   if (!isApplianceType(applianceType) || applianceType === null) {
     return [];
   }
@@ -42,6 +59,8 @@ async function getByType(applianceType: ApplianceTypes) {
   const appliances = await prisma.appliances.findMany({
     where: { type: applianceType },
     select: ApplianceSelectConfig,
+    take,
+    skip,
   });
 
   return appliances;
