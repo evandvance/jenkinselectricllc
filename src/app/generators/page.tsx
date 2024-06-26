84a6f6009ec3b92b464de9a71302c62ad23010b4
evandvance <evandvance@gmail.com>
2024-06-26 15:59:27 -0500

    Update to generator page

--- src/app/generators/page.tsx
+++ src/app/generators/page.tsx
@@ -1,8 +1,17 @@
-import ComingSoonCard from '@/components/Cards/ComingSoonCard';
-const GeneratorPage = () => {
+import { GeneratorTypes } from '@prisma/client';
+import GeneratorDisplay from '@/components/Displays/GeneratorDisplay';
+
+interface GeneratorPageProps {
+  searchParams: {
+    brand?: GeneratorTypes;
+  };
+}
+
+const GeneratorPage = ({ searchParams: { brand } }: GeneratorPageProps) => {
+  console.log(brand);
   return (
-    <div>
-      <ComingSoonCard />
+    <div className="flex flex-col justify-center items-center">
+      <GeneratorDisplay brand={brand} />
     </div>
   );
 };
--- src/interfaces/GeneratorInterface.ts
+++ src/interfaces/GeneratorInterface.ts
@@ -1,6 +1,6 @@
 import { ApplianceImages, Generators, Reservations } from '@prisma/client';
 
 export interface generatorsInterface extends Generators {
-  images: ApplianceImages;
-  reservations?: Reservations;
+  images: ApplianceImages[];
+  reservation?: Reservations;
 }
