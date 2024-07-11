a0396edd26a42d14e3705bc900c3fb13cda99d0d
evandvance <evandvance@gmail.com>
2024-07-11 14:22:25 -0600

    Add permit instruction display to page

--- src/app/info/permits/page.tsx
+++ src/app/info/permits/page.tsx
@@ -1,8 +1,10 @@
-import ComingSoonCard from '@/components/Cards/ComingSoonCard';
+import PermitCardDisplay from '@/components/Displays/PermitCardDisplay';
+
 const PermitPage = () => {
   return (
-    <div>
-      <ComingSoonCard />
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl m-5">How to pull a permit</h1>
+      <PermitCardDisplay />
     </div>
   );
 };
