2d94c6738b6138344d60c4917474662a7a6e9d95
Evan Vance <72946268+evandvance@users.noreply.github.com>
2025-04-08 14:04:22 -0500

    Use card on contact Page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import ContactCard from '@/components/Cards/ContactCard';
-import BlueButton from '@/components/Buttons/BlueButton';
-import FadeIn from '@/components/Utilities/FadeIn';
+import ContactCard from "@/components/Cards/ContactCard";
+import BlueButton from "@/components/Buttons/BlueButton";
+import ContactClusterCard from "@/components/Cards/ContactClusterCard";
+import FadeIn from "@/components/Utilities/FadeIn";
 
 const ContactPage = () => {
   return (
@@ -19,37 +19,7 @@ const ContactPage = () => {
           target="_blank"
         />
       </FadeIn>
-      <FadeIn>
-        <div className="text-2xl space-y-2 w-[90%] lg:w-auto">
-          <p>
-            Phone Number:{' '}
-            <Link
-              className="text-jellcblue hover:underline underline-offset-2"
-              href="tel:7317276578"
-            >
-              (731) 727-6578
-            </Link>
-          </p>
-          <p>
-            Email:{' '}
-            <Link
-              className="text-jellcblue hover:underline underline-offset-2"
-              href="mailto:jenkinselectric96@gmail.com"
-            >
-              jenkinselectric96@gmail.com
-            </Link>
-          </p>
-          <p>
-            Address:{' '}
-            <Link
-              className="text-jellcblue hover:underline underline-offset-2"
-              href={'https://maps.app.goo.gl/dUynoe9JCxRqiF5i6'}
-            >
-              357 W Main St, Adamsville, TN 38310
-            </Link>
-          </p>
-        </div>
-      </FadeIn>
+      <ContactClusterCard />
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3258.934441422707!2d-88.3993759241365!3d35.23300197273523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x887c4c9c0a9a3a6d%3A0x9670eb45a4f8f0f9!2s357%20W%20Main%20St%2C%20Adamsville%2C%20TN%2038310!5e0!3m2!1sen!2sus!4v1719333952388!5m2!1sen!2sus"
         loading="lazy"
